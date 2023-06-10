467d39b9474fa1fcad1085ce2ab1bec1f34943ef
jopajooi <seltkol@gmail.com>
2023-06-10 11:55:06 +0300

    dz5

---
+++ Adventure1/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+
+
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        do
+        {
+            array[i] = new Random().Next(-999, 1000);
+        }
+        while (array[i] < 99 && array[i] > -99);
+    }
+    System.Console.WriteLine("Массив:  ");
+}
+int EvenNumbers(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+
+    }
+    return count;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (var item in array)
+    {
+        System.Console.Write($"{item} ");
+    }
+    System.Console.WriteLine();
+}
+
+System.Console.WriteLine("Enter length array:   ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] Array = new int[size];
+
+
+FillArray(Array);
+EvenNumbers(Array);
+PrintArray(Array);
+
+System.Console.WriteLine($"Количество чётных чисел в массиве имеет значение:  {EvenNumbers(Array)}");
---
+++ Adventure2/Program.cs
@@ -0,0 +1,49 @@
+﻿
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных индексах.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+System.Console.WriteLine("Enter length array:   ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] Array = new int[size];
+
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+
+        array[i] = new Random().Next();
+    }
+    System.Console.WriteLine("Массив:  ");
+}
+int NoEvenNumbers(int[] array)
+{
+    int sum = 0;
+    for (int i = 1; i < size; i += 2)
+    {
+
+        sum += array[i];
+    }
+
+    return sum;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (var item in array)
+    {
+        System.Console.Write($"{item} ");
+    }
+    System.Console.WriteLine();
+}
+
+
+FillArray(Array);
+NoEvenNumbers(Array);
+PrintArray(Array);
+
+System.Console.WriteLine($"Сумма нечётных чисел в массиве имеет значение:  {NoEvenNumbers(Array)}");
